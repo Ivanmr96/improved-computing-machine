5a2155e697db69d20d62eba1b831751f91646085
Iván <44922373+Ivanmr96@users.noreply.github.com>
2020-01-23 18:38:27 +0100

    Modelos y ViewModel de la sala de espera añadidos

---
+++ Pictionary/Pictionary/ViewModel/SalaEsperaVM.cs
@@ -0,0 +1,81 @@
+﻿using Pictionary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictionary.ViewModel
+{
+    class SalaEsperaVM
+    {
+        #region atributos privados
+
+        private clsPartida partida;
+        private clsMensaje mensaje;
+
+        #endregion
+
+        #region constructor
+
+        public SalaEsperaVM()
+        {
+            // Aquí obtendría la partida enviada desde la otra ventana
+
+            partida = new clsPartida();
+
+            partida.ListadoJugadores.Add(new clsJugador("id", 0, "Ivan", false, false, false));
+            partida.ListadoJugadores.Add(new clsJugador("id", 0, "Pepe", false, false, false));
+        }
+
+        #endregion
+
+        #region propiedades publicas
+
+        public clsPartida Partida
+        {
+            get => partida;
+            set => partida = value;
+        }
+        public clsMensaje Mensaje
+        {
+            get => mensaje;
+            set => mensaje = value;
+        }
+
+        #endregion
+
+        #region commands
+
+        public Comando enviarMensaje { get; set; }
+
+        private bool enviarMensaje_canExecute() => mensaje != null || mensaje.Mensaje != "";
+
+        private void enviarMensaje_execute()
+        {
+            //Mandar el mensaje al servidor
+        }
+
+        public Comando salir { get; set; }
+
+        private void salir_execute()
+        {
+            //Indica al serivdor que sale.
+        }
+
+        public Comando comenzarPartida { get; set; }
+
+        public bool comenzarPartida_canExecute()
+        {
+            //Puede ejecutar si es el lider
+            return true;
+        }
+
+        public void comenzarPartida_execute()
+        {
+            //Indica al servidro que la partida va a comenzar.
+        }
+
+        #endregion
+    }
+}
