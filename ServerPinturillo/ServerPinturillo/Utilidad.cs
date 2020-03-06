12821096f51cee14d34a4a52afb354713d940ff5
Angela <angyvazquezdel97@gmail.com>
2020-03-06 09:50:44 +0100

    he añadido palabras

--- ServerPinturillo/ServerPinturillo/Utilidad.cs
+++ ServerPinturillo/ServerPinturillo/Utilidad.cs
@@ -12,19 +12,46 @@ namespace ServerPinturillo
 
         //private static String[] listadoPalabras = { "Ratón y mouse", "Teclado y otra cosa" };
 
-        private static String[] listadoPalabras = {"Ratón", "Teclado", "Monitor", "Profesor", "Movil", "Océano", "Mar", "Playa", "Persona", "Igual",
-        "Saludar", "Hucha","Libro", "Vaso", "Plátano", "Ventana", "Pañuelo", "Gato", "Perro", "Loro", "Dignidad",
-        "Mochila", "Muralla", "Android", "Cámara", "Brazo", "Hilo", "Twitter", "Instagram","Barco","Estrella",
-        "Youtube", "Música", "Escaleras", "Platos", "Cocina", "Ojo", "Cine", "Butaca","Cohete","Selva",
-        "Viaje","Jazz","Caballo","Tortuga","Elefante","Mapache","Mono","Pulpo","Cine","Mar","Globo",
-        "Rectángulo","Círculo","Ordenador","Pizarra","Darth Vader","Ironman","Capitán América","Hulk",
-        "Superman","Batman","Thor","Spiderman","Thanos","Flash","Cebolla", "Tarjeta", "Pegatina", "Cristal",
-        "Mopa", "Lápiz", "Saltamontes", "Hormiga", "Proyector", "Pimiento", "Autobus", "Coche", "Vaca", "Burro",
-        "Cacatúa", "Luna", "Amor", "Corazón", "Melón", "Sandía", "Kiwi", "Foto", "Camiseta", "Pantalones",
-        "Atenea", "Zeus", "Lavadora", "Lavavajillas", "Fregadero", "Sofá", "Padre", "Dios", "Madre", "Botella",
-        "Tomate", "Columna", "Fila", "Nevera", "Taza", "Cuenco", "Arroz", "Lechuga", "Aire acondicionado", 
-        "Calentador", "Cuadro", "Televisión", "Portugal", "España", "Francia", "Silla", "Sevilla"};
+        private static String[] listadoPalabras = {
 
+        "Ratón", "Teclado", "Monitor", "Profesor", "Movil", "Océano", "Mar", "Playa", "Persona", "Igual", "Saludar",
+            "Hucha","Libro", "Vaso", "Plátano", "Ventana", "Pañuelo", "Gato", "Perro", "Loro", "Dignidad",
+        "Mochila", "Muralla", "Android", "Cámara", "Brazo", "Hilo", "Twitter", "Instagram","Barco","Estrella",
+            "Youtube", "Música", "Escaleras", "Platos", "Cocina", "Ojo", "Cine", "Butaca","Cohete","Selva",
+        "Viaje","Jazz","Caballo","Tortuga","Elefante","Mapache","Mono","Pulpo","Cine","Mar","Globo", "Rectángulo",
+            "Círculo","Ordenador","Pizarra","Darth Vader","Ironman","Capitán América","Hulk", "Superman","Batman",
+            "Thor","Spiderman","Thanos","Flash","Cebolla", "Tarjeta", "Pegatina", "Cristal", "Mopa", "Lápiz", "Saltamontes",
+            "Hormiga", "Proyector", "Pimiento", "Autobus", "Coche", "Vaca", "Burro",
+        "Cacatúa", "Luna", "Amor", "Corazón", "Melón", "Sandía", "Kiwi", "Foto", "Camiseta", "Pantalones", "Atenea", "Zeus",
+            "Lavadora", "Lavavajillas", "Fregadero", "Sofá", "Padre", "Dios", "Madre", "Botella",
+        "Tomate", "Columna", "Fila", "Nevera", "Taza", "Cuenco", "Arroz", "Lechuga", "Aire acondicionado", "Calentador", "Cuadro",
+            "Televisión", "Portugal", "España", "Francia", "Silla", "Sevilla",
+        "Agujero negro", "Palmera", "Moflete", "Imán", "Vacuna", "Planeta", "Tierra", "Espacio", "Sol", "Miel", "Clavo", "Espectador",
+            "Campo", "Medalla", "Frambuesa", "Hamburguesa", "Deportista", "Antebrazo", "Cabeza","Cuerpo", "Tórax",
+        "Pulmón", "Plumas", "Grillo", "Cojín", "Pájaro", "Estornino", "León", "Guepardo", "Gacela", "Leopardo", "Buitre",
+            "Ave carroñera", "Cielo", "Nube", "Bisagra", "Destornillador", "Anillo", "Boda", "Comunión", "Viaje", "Pulso",
+            "Pulsera", "Agenda", "Cable", "Hoja",
+        "Árbol", "Madera", "Flor", "Rosa", "Naranja", "Comedia", "Dragón", "Sangre", "Piel", "Ser humano", "Apisonadora",
+            "Película", "Pelícano", "Gorrión", "Frío", "Calor", "Templado", "Templo", "Iglesia", "Mezquita", "Cerrado", "Enero",
+            "Mes", "Tiempo", "Legendario", "Leyenda",
+        "Mastodonte", "Fiera", "Sable", "Atajo", "Tubérculo", "Patata", "Periscopio", "Cangrejo", "Concha", "Dinamita",
+            "Navidad", "Frente",
+        "Carrera", "Trucha", "Lobo", "Asesinato", "Asesino", "Muerte", "Enfermedad", "Anciano", "Consola", "Audífono", "Reno", "Papá Noel",
+        "Ombligo", "Disquete", "Cronómetro", "Juego", "Termostato", "Sello", "Fiesta", "Lejos", "Cerca", "Barrera", "Hospital", "Virus", "Juego de mesa", "Mesa",
+        "Rastro", "Rata", "Careta", "Carnaval", "Batidora", "Horno", "Microondas", "Instrumentos", "Saxofón", "Piano", "Química", "Física", "Matemáticas",
+        "Lengua", "Biología", "Informática", "Juicio", "Primero", "Gasa", "Norte", "Sur", "Este", "Oeste", "Rosa de los vientos", "Vista", "Oreja",
+        "Helado", "Remolque", "Motor", "Gotera", "Gota", "Paella", "Lava", "Monja", "Cura", "Mermelada", "Llanto", "Llorar", "Sacrificio",
+            "Barco pesquero", "Veneno", "Tóxico", "Calavera", "Hueso", "Cocodrilo", "Calculadora", "Cómic", "Carta", "Mensaje", "Espejo", "Espejismo"
+                
+
+        };
+
+
+        /// <summary>
+        /// Método que devuelve una palabra
+        /// aleatoria del array de palabras
+        /// </summary>
+        /// <returns>palabra del array de palabras</returns>
         public static String obtenerPalabraAleatoria()
         {
             Random rnd = new Random();
